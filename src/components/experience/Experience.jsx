c12fb3e29e30b2e970573c8699d8c1b2646a75ae
naveed ali abbasi <getinfo.naveed@gmail.com>
2025-03-18 16:51:51 +0500

    set experience certificate

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import certificate from '../../assets/certificate.jpg'
+// import certificate from '../../assets/certificate.jpg'
 
 export default function Experience() {
   return (
@@ -11,7 +11,7 @@ export default function Experience() {
         <div className='container m-auto flex px-8 py-24 md:flex-row flex-col items-center justify-center'>
             <div data-aos='fade-right' data-aos-delay='400' className='relative lg:max-w-lg lg:w-full md:w-1/2 w-5/6 md:mb-0 '>
                 <div className='md:h-[450px] h-[400px] md:w-[300px] w-[300px] md:right-36 bg-gradient-to-r from-blue-600 via-indigo-500 to-blue-400 absolute rounded-md transform rotate-3 -top-8 shadow-[0_0_40px_rgba(59,130,246,0.7)] z-0'></div>
-                <img src={certificate} className='object-contain w-[300px] h-[450px] rounded relative z-10 block m-auto' alt="Experience" />
+                <img src={" "} className='object-contain w-[300px] h-[450px] rounded relative z-10 block m-auto' alt="Experience" />
             </div>
             <div data-aos='fade-left' data-aos-delay='400' className='lg:flex-grow md:w-1/2 lg:pl-8 md:pl-8 flex flex-col md:items-start md:text-left items-center text-center'>
                 <h1 className='title-font sm:text-4xl text-3xl mb-4 mt-7 font-medium text-blue-400'>Experience</h1>
